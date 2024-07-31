523f826eb51e780d0ef1d3e4584bf122685e2d8d
XdotCore <xandercorvalan@gmail.com>
2024-07-31 11:41:49 -0500

    Add android soft-keyboard fix

--- PassSync/Platforms/Android/MainActivity.cs
+++ PassSync/Platforms/Android/MainActivity.cs
@@ -1,10 +1,15 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using MauiBlazor3.Platform.Android;
 using Microsoft.Maui;
 
 namespace PassSync {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity {
+        protected override void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+            WebViewSoftInputPatch.Initialize();
+        }
     }
 }
